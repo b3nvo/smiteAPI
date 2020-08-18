880a53ec3ce1c15cfc59a58b0ad62a5fd94ec9d2
Bas Voss <voss.bas@gmail.com>
2020-08-18 17:53:07 +0200

    add no endpoint error

--- server.js
+++ server.js
@@ -22,5 +22,8 @@ access.connect((err, resp) => {
 })
 
 app.use('/api', routes);
+app.all('*', (req, res) => {
+    res.status(404).json({ message: 'Endpoint not found'});
+})
 
 module.exports = app;
