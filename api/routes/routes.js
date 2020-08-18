14a790c1a233190c3383afba62b51837d803ed6a
Bas Voss <voss.bas@gmail.com>
2020-08-18 17:54:57 +0200

    new api call in routes

--- api/routes/routes.js
+++ api/routes/routes.js
@@ -4,7 +4,7 @@ const itemRoutes = require('./item.routes');
 const playerRoutes = require('./player.routes');
 const Api = require('../api');
 const global = require('../global');
-const api = new Api(global.config.devId, global.config.authKey);
+const api = new Api();
 
 router.get('/test', (req, res) => api.test());
 router.get(
